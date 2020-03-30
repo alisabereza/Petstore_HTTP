ec4413c2a5f4186732b94abed47b91161dc9149d
alisabereza <alisabereza@gmail.com>
2020-03-30 19:02:44 +0300

    EntityType added (pet, shop, user).

---
+++ src/main/java/com/berezovska/petstore/view/services/EntityType.java
@@ -0,0 +1,26 @@
+package com.berezovska.petstore.view.services;
+
+import com.berezovska.petstore.model.PetStatus;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum EntityType {
+    PET("pet"), SHOP("shop"), USER("user");
+
+    private String type;
+
+    EntityType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static Optional<EntityType> getType(String type) {
+        return Arrays.stream(EntityType.values())
+                .filter(enumValue -> enumValue.getType().equals(type))
+                .findAny();
+    }
+}
